a12fa28c9cf6b53ad28ecaa154bedd3b2bdda85e
michalZ67214 <w67214@student.wsiz.edu.pl>
2023-05-10 23:00:15 +0200

    Lab_2_zadania_rekurencja - zadanie 3

---
+++ Lab_2/Zadania_rekurencja/3/3.py
@@ -0,0 +1,13 @@
+def f(n):
+    if n == 1:
+        return 4
+    else:
+        return 1 / (1 - f(n-1))
+
+n = int(input('Podaj n: '))
+
+while n < 1:
+    print()
+    n = int(input('Podaj n: '))
+
+print(f(n))
